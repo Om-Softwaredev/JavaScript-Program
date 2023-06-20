ff197bcec4bc56d87737b49371f8e71d655377b0
Omprakash <omprakash8578502@gmail.com>
2023-06-20 07:09:57 +0530

    dataType commit

---
+++ DataTypes_Operators/test.js
@@ -0,0 +1,37 @@
+// var name;//Declaration
+// name="om";//Initialization
+
+// var num1=10;
+// var num2=30;
+ 
+// var result= num1+num2;//Expresion evaluation
+
+// console.log(name);
+// console.log(result);
+
+// var num1="10";
+// var num2="30";
+
+// var result= num1+num2;
+// console.log(result);
+
+// var num1=10;
+// num1=15;
+// let num2=30;
+// num2=35;
+// const num3=40;
+// // num3=50;// we can't reassign value to const variable
+
+// console.log(num1);
+// console.log(num2);
+// console.log(num3);
+
+var n="om"
+var num=10;
+console.log(typeof n);
+console.log(typeof num.toString());
+
+var value= 13.875;
+console.log(value.toFixed());
+console.log(Number("50"));
+console.log(parseInt("10 years"));//10
