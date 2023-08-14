b36e824cea72a7ccf559e051f283a637f8670ac5
Omprakash <omprakash8578502@gmail.com>
2023-08-14 15:45:09 +0530

    conditional statements

---
+++ Conditional/index.js
@@ -0,0 +1,40 @@
+// if(true){
+//     console.log("Inside if block");
+// }
+
+
+if(false){
+    console.log("Inside if block");
+}else{
+    console.log("Inside else block");
+}
+
+console.log("Outside if block");
+
+//Ternary
+    // conditionalCheck?"true":"false"
+
+// Switch
+
+let day=5;
+switch (day) {
+    case 1:
+        console.log("Monday");
+        break;
+    case 2:
+        console.log("Tuesday");
+        break;
+    case 3:
+        console.log("Wednesday");
+        break;
+    case 4:
+        console.log("ThursDay");
+        break;
+    case 5:
+        console.log("Friday");
+        break;
+
+    default:
+        console.log("Holiday");
+        break;
+}
